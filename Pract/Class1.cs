4514670e4a51af6d4b293f77bc18cdbcf0769ce3
artem <artem@BOOK-ARTEM>
2024-04-04 19:21:28 +0700

    Добавьте файлы проекта.

---
+++ Pract/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pract
+{
+    internal class Class1
+    {
+        public static Pract.Class1 Instance { get; set; }
+
+
+    }
+}
---
+++ Pract/Pages/Auto.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pract.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Auto.xaml
+    /// </summary>
+    public partial class Auto : Page
+    {
+        public Auto()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEnter_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtbLogin.Text == "admin" && txtPassword.Text == "1234")
+            {
+                NavigationService.Navigate(new Admin());
+            }
+            else
+            {
+                MessageBox.Show("Вы ввели неверные данные повторите попытку");
+            }
+        }
+    }
+}
